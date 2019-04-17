57028717f5f47224b38e823606fdc124b3a9056f
Prathap <25050648+d3xt3r0@users.noreply.github.com>
2019-04-17 22:30:37 +0530

    Add files via upload

---
+++ CommandLineArgument1.java
@@ -0,0 +1,7 @@
+class CommandLineArgument1{
+	public static void main(String[] args){
+		String word1 = args[0];
+		String word2 = args[1];
+		System.out.println(word1 + " Technologies "  + word2);
+	}
+}
